57f97f68b3de4dadbe24d2916a681e666494ca6c
Warlyn <warlyn200@gmail.com>
2021-12-10 14:36:00 -0400

    Update PokemonCard.jsx

--- src/components/Organisms/PokemonCard.jsx
+++ src/components/Organisms/PokemonCard.jsx
@@ -3,19 +3,18 @@ import { Link } from 'react-router-dom';
 
 function PokemonCard({ tipoPrimario, tipoSecundario, nombre, descripcion, imagen, id }) {
     return (
-        <div class="col-md-6">
-            <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-                <div class="col p-4 d-flex flex-column position-static">
-                    <strong class="d-inline-block mb-2 text-primary">{tipoPrimario + '/' + tipoSecundario}</strong>
-                    <h3 class="mb-0">{nombre}</h3>
-                    <p class="card-text mb-auto"> {descripcion} </p>
-                    <a href="#" class="stretched-link"></a>
-                    <Link to={'/pokemon/' + id} class="stretched-link" />
+        <div className="col-md-6">
+            <div className="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+                <div className="col p-4 d-flex flex-column position-static">
+                    <strong className="d-inline-block mb-2 text-primary">{tipoPrimario + '/' + tipoSecundario}</strong>
+                    <h3 className="mb-0">{nombre}</h3>
+                    <p className="card-text mb-auto"> {descripcion} </p>
+                    <Link to={'/pokemon/' + id} className="stretched-link" />
                 </div>
 
-                <div class="col-auto d-none d-lg-block">
+                <div className="col-auto d-none d-lg-block">
                     <img
-                        class="bd-placeholder-img" width="200" height="250"
+                        className="bd-placeholder-img" width="200" height="250"
                         src={imagen}
                         alt={nombre}
                     />
