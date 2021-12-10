bba0525854cce97b4b1b2060182c65245b12ad7d
platinumplace <warlyn200@gmail.com>
2021-12-10 13:34:30 -0400

    ultimos detalles de prueba

--- src/components/Templates/PokedexGrid.jsx
+++ src/components/Templates/PokedexGrid.jsx
@@ -2,15 +2,37 @@ import React from 'react'
 import PokemonCard from '../Organisms/PokemonCard';
 
 function PokedexGrid() {
+
+    const pokedex = [
+        {
+            "tipoPrimario": "Rayo",
+            "tipoSecundario": "Rayo",
+            "nombre": "Rayo",
+            "descripcion": "Rayo",
+            "imagen": "https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png"
+        },
+        {
+            "tipoPrimario": "Rayo",
+            "tipoSecundario": "Rayo",
+            "nombre": "Rayo",
+            "descripcion": "Rayo",
+            "imagen": "https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png"
+        }
+    ]
+
     return (
         <div class="row mb-2">
-            <PokemonCard
-                tipoPrimario="Rayo"
-                tipoSecundario="Agua"
-                nombre="Pikachu"
-                descripcion="le gusta correr"
-                imagen="https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png"
-            />
+            {
+                pokedex.map(pokemon =>
+                    <PokemonCard
+                        tipoPrimario={pokemon.tipoPrimario}
+                        tipoSecundario={pokemon.tipoSecundario}
+                        nombre={pokemon.nombre}
+                        descripcion={pokemon.descripcion}
+                        imagen={pokemon.imagen}
+                    />
+                )
+            }
         </div>
     );
 }
